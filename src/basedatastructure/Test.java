12935549d32e42cf55e3ccee8530184197160ace
July24 <395513427@qq.com>
2019-08-23 10:24:33 +0800

    删除outâ

--- src/basedatastructure/Test.java
+++ src/basedatastructure/Test.java
@@ -4,57 +4,85 @@ import java.util.Iterator;
 
 public class Test {
     public static void main(String[] args) {
-        AbstractSymbolTable<String, Integer> symbolTable = new ParallelArrayST<>();
-        symbolTable.put("f", 6);
-        symbolTable.put("g", 7);
+        TwoThreeSearchTreeST<String, Integer> symbolTable = new TwoThreeSearchTreeST<>();
         symbolTable.put("a", 1);
+        symbolTable.put("f", 6);
         symbolTable.put("b", 2);
         symbolTable.put("h", 8);
         symbolTable.put("i", 9);
         symbolTable.put("c", 3);
         symbolTable.put("d", 4);
-        symbolTable.put("e", 5);
         symbolTable.put("j", 10);
         symbolTable.put("k", 11);
-        Iterator<String> keys = symbolTable.keys();
-        while(keys.hasNext()) {
-            String key = keys.next();
-            System.out.println("key:" + key + "  value:" + symbolTable.get(key));
+        symbolTable.put("z", 99);
+//        System.out.println(symbolTable.get("j"));
+//        System.out.println(symbolTable.get("h"));
+//        System.out.println(symbolTable.get("b"));
+//        System.out.println("总数" + symbolTable.size());
+//        symbolTable.put("j", 66);
+//        System.out.println(symbolTable.get("j"));
+//        System.out.println("总数" + symbolTable.size());
+        Iterator iter = symbolTable.keys();
+        while(iter.hasNext()) {
+            System.out.println(iter.next());
         }
-        symbolTable.put("c", 30);
-        System.out.println("value:" + symbolTable.get("b"));
-        System.out.println("size:" + symbolTable.size());
-        System.out.println("isEmpty:" + symbolTable.isEmpty());
-        System.out.println("value:" + symbolTable.get("c"));
-        System.out.println("contain c:" + symbolTable.contains("c"));
-        symbolTable.delete("c");
-        System.out.println("contain c:" + symbolTable.contains("c"));
-        keys = symbolTable.keys();
-        while(keys.hasNext()) {
-            String key = keys.next();
-            System.out.println("key:" + key + "  value:" + symbolTable.get(key));
-        }
-        System.out.println("size:" + symbolTable.size());
-        symbolTable.delete("c");
-        symbolTable.delete("d");
-        symbolTable.delete("e");
-        symbolTable.delete("f");
-        symbolTable.delete("g");
-        symbolTable.delete("h");
-        symbolTable.delete("i");
-        symbolTable.delete("jjjj");
-        System.out.println("size:" + symbolTable.size());
+//        Iterator iterator = symbolTable.keys("d","i");
+//        while(iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+//        symbolTable.print();
+//        System.out.println(symbolTable.select(2) + "  size:" + symbolTable.size());
+////        symbolTable.delete("z");
+////        System.out.println(symbolTable.select(10) + "  size:" + symbolTable.size());
+//        symbolTable.delete("c");
+//        System.out.println(symbolTable.select(2) + "  size:" + symbolTable.size());
+//        symbolTable.delete("e");
+//        System.out.println(symbolTable.select(3) + "  size:" + symbolTable.size());
+//        symbolTable.delete("j");
+//        System.out.println(symbolTable.select(7) + "  size:" + symbolTable.size());
 
+//        Iterator<String> keys = symbolTable.keys();
+//        while(keys.hasNext()) {
+//            String key = keys.next();
+//            System.out.println("key:" + key + "  value:" + symbolTable.get(key));
+//        }
+//        symbolTable.put("c", 30);
+//        System.out.println("value:" + symbolTable.get("b"));
+//        System.out.println("size:" + symbolTable.size());
+//        System.out.println("isEmpty:" + symbolTable.isEmpty());
+//        System.out.println("value:" + symbolTable.get("c"));
+//        System.out.println("contain c:" + symbolTable.contains("c"));
+//        symbolTable.delete("c");
+//        System.out.println("contain c:" + symbolTable.contains("c"));
+//        keys = symbolTable.keys();
+//        while(keys.hasNext()) {
+//            String key = keys.next();
+//            System.out.println("key:" + key + "  value:" + symbolTable.get(key));
+//        }
+//        System.out.println("size:" + symbolTable.size());
+//        symbolTable.delete("c");
+//        symbolTable.delete("d");
+//        symbolTable.delete("e");
+//        symbolTable.delete("f");
+//        symbolTable.delete("g");
+//        symbolTable.delete("h");
+//        symbolTable.delete("i");
+//        symbolTable.delete("jjjj");
+//        System.out.println("size:" + symbolTable.size());
 
-//        BinaryHeapImpPQ traverseImpPQ = new BinaryHeapImpPQ(QueueType.MIN);
-//        traverseImpPQ.insert(1);
-//        traverseImpPQ.insert(60);
-//        traverseImpPQ.insert(55);
-//        traverseImpPQ.insert(40);
-//        traverseImpPQ.insert(2);
-//        traverseImpPQ.insert(34);
-//        traverseImpPQ.insert(76);
-//        traverseImpPQ.insert(88);
+//        IndexPQ<String> traverseImpPQ = new IndexPQ(20, QueueType.MIN);
+//        traverseImpPQ.insert(1, "a");
+//        traverseImpPQ.insert(7,"g");
+//        traverseImpPQ.insert(2, "b");
+//        traverseImpPQ.insert(8,"h");
+//        traverseImpPQ.insert(3, "c");
+//        traverseImpPQ.insert(5,"e");
+//        traverseImpPQ.insert(6,"f");
+//        traverseImpPQ.insert(4, "d");
+//
+//
+//        traverseImpPQ.change(3,"u");
+//        traverseImpPQ.change(5,"w");
 //        System.out.println(traverseImpPQ.takeOut());
 //        System.out.println(traverseImpPQ.takeOut());
 //        System.out.println(traverseImpPQ.takeOut());
